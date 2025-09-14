829e765edf1cfde282f456922acc3e4f5fbfce96
LINDECKER Charles <charles.lindecker@outlook.fr>
2025-09-14 11:23:14 +0200

    fix: prechargement de la video pour gagner en fluidité

--- shrek.js
+++ shrek.js
@@ -3,23 +3,24 @@
     const btns = document.querySelectorAll(".shrek-troll");
     if (!btns.length) return;
 
+    const vid = document.createElement("video");
+    vid.src = videoSrc;
+    vid.autoplay = true;
+    vid.loop = true;
+    vid.controls = false;
+    vid.style.position = "fixed";
+    vid.style.top = 0;
+    vid.style.left = 0;
+    vid.style.width = "100%";
+    vid.style.height = "100%";
+    vid.style.objectFit = "cover";
+    vid.style.zIndex = 9999;
+
     btns.forEach((btn) => {
       btn.addEventListener("click", () => {
         // Remplace tout le body
         document.body.innerHTML = "";
         // Crée la vidéo
-        const vid = document.createElement("video");
-        vid.src = videoSrc;
-        vid.autoplay = true;
-        vid.loop = true;
-        vid.controls = false;
-        vid.style.position = "fixed";
-        vid.style.top = 0;
-        vid.style.left = 0;
-        vid.style.width = "100%";
-        vid.style.height = "100%";
-        vid.style.objectFit = "cover";
-        vid.style.zIndex = 9999;
         document.body.appendChild(vid);
 
         // Débloque le son
