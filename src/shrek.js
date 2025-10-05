dd9ed53ed48f8705a67bb15602b7e1435d518635
LINDECKER Charles <charles.lindecker@outlook.fr>
2025-10-05 17:31:36 +0200

    fix : l'ajoute sur les deux scripts

--- src/shrek.js
+++ src/shrek.js
@@ -1,12 +1,13 @@
 /* global unleashShrekOnAll */
 import { loadvideo } from './lib/video.js';
-import { getLinks, setAllLink } from './lib/dom.js'; 
+import { getLinks, setAllLink, cleanLink } from './lib/dom.js'; 
 
 (function () {
   function unleashShrekOnAll() {
     const btns = getLinks(".shrek-troll");
     if (!btns.length) return;
     const vid = loadvideo();
+    cleanLink(btns);
     setAllLink(btns, vid);
   }
 
